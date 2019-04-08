9e6b80a5b88ccecfd081662b4dcf168c05dba0b6
Cristian Raña <cristian.rana@despegar.com>
2019-04-08 20:45:51 -0300

    Algorithm ready

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 
 const { ActivityTypes } = require('botbuilder');
+const exec = require('child_process').execSync;
+const fs = require('fs');
 
 class MyBot {
     /**
@@ -11,7 +13,16 @@ class MyBot {
     async onTurn(turnContext) {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         if (turnContext.activity.type === ActivityTypes.Message) {
-            await turnContext.sendActivity(`You said '${ turnContext.activity.text }'`);
+            fs.writeFileSync("temp", `${turnContext.activity.text}`, () => {});
+            let output = exec(`textrank -t temp`, (err, stdout, stderr) => {
+                if (err) {
+                    console.log("Couldn't execute command.");
+                    console.log(`stdout: ${ stdout }`);
+                    console.log(`stderr: ${ stderr }`);
+                }
+            });
+            await turnContext.sendActivity(`${ output }`);
+            fs.unlinkSync("temp")
         } else {
             await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
         }
