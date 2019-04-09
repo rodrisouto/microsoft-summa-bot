e22bc0c5362efb37abdd57719543ca240ae23baf
Rodrigo Souto <rnsoutob@gmail.com>
2019-04-09 11:45:58 -0300

    Passing to functions

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ const exec = require('child_process').execSync;
 const fs = require('fs');
 const wtf = require('wtf_wikipedia');
 
+let TMP_FILE_FOR_SUMMA_API = 'temp';
 
 class SummaBot {
     /**
@@ -16,33 +17,8 @@ class SummaBot {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         if (turnContext.activity.type === ActivityTypes.Message) {
 
-            /*
-            var wikiText = await (async () => {
-                var doc = await wtf.fetch('Toronto');
-                return await doc.text();
-            })();
-            */
             var wikiText = await this.getTextFromWikipedia(`${ turnContext.activity.text }`);
-
-
-                // !!!!
-            /*
-            console.log(wikiText);
-            await turnContext.sendActivity(`${ wikiText }`);
-            */
-
-            fs.writeFileSync(`temp`, `${ wikiText }`, () => {
-            });
-            let output = exec(`textrank -t temp -w 200`, (err, stdout, stderr) => {
-                if (err) {
-                    console.log("Couldn't execute command.");
-                    console.log(`stdout: ${ stdout }`);
-                    console.log(`stderr: ${ stderr }`);
-                }
-            });
-            await turnContext.sendActivity(`${ output }`);
-            fs.unlinkSync("temp");
-
+            await this.sendSummaText(turnContext, wikiText);
 
         } else {
             await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
@@ -51,10 +27,25 @@ class SummaBot {
 
     async getTextFromWikipedia(title) {
 
-        return await (async () => {
+        return (async () => {
             var doc = await wtf.fetch(`${ title }`);
             return await doc.text();
         })();
+    }
+
+    async sendSummaText(turnContext, text) {
+
+        fs.writeFileSync(TMP_FILE_FOR_SUMMA_API, `${ text }`, () => {
+        });
+        let output = exec(`textrank -t temp -w 200`, (err, stdout, stderr) => {
+            if (err) {
+                console.log("Couldn't execute command.");
+                console.log(`stdout: ${ stdout }`);
+                console.log(`stderr: ${ stderr }`);
+            }
+        });
+        await turnContext.sendActivity(`${ output }`);
+        fs.unlinkSync(TMP_FILE_FOR_SUMMA_API);
     }
 }
 
